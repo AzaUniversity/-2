1205d8736bc3eb76baa41e2143c975619ad26823
AzaUniversity <azamasha0410@gmail.com>
2024-08-23 15:21:35 +0300

    Add files via upload

---
+++ Строки и интеграция строк.py
@@ -0,0 +1,6 @@
+exemple = 'Urban'
+print(exemple[0]) # 1 символ строки
+print(exemple[-1]) # Псоледний символ
+print(exemple[2:]) # Половина строки
+print(exemple[::-1]) # Задом наперед
+print(exemple[::2]) # Каждый 2 символ
